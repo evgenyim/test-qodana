8763e7f36434b2c67f2b392ea036e6be2f9ea746
Evgenii Im <evgenii.im@jetbrains.com>
2025-04-24 13:26:51 +0200

    update files

--- Main.kt
+++ Main.kt
@@ -1,11 +1,11 @@
 fun main() {
     val a = 1
     val b = 2
-    val c = a + b + b
+    var c = a + b + b
     println("Tesst2 + added")
     println(c)
     val d: Int
     while (c == 5) {
-
+        println(c++)
     }
 }
